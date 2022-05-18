49d498df713659564fec1c0cf56c7c5b76c8fcb7
Daryna_Korotych <darynakorotych@gmail.com>
2022-05-18 13:45:25 +0300

    Connected nodejs

---
+++ js/test.js
@@ -0,0 +1,55 @@
+/* 'use strict';
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c'
+};
+
+const b = 'asd';
+
+// arrObj.b = 'qwerty';
+// arrObj['b'] = 'qwerty';
+arrObj[b] = 'qwerty';
+
+// console.log(arrObj['b']);
+console.log(arrObj.b);
+console.log(arrObj); */
+// ===============================================
+/* let storeName = 'HatHat';
+const storeDescription = {
+    budget: 10000,
+    employees: ['Ivan', 'Jack', 'Maria'],
+    products: {
+        hatSmall: 150,
+        hatBig: 300
+    },
+    open: false
+};
+
+console.log(storeDescription.open); */
+// ==============================================
+/* const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your first name?', '');
+answers[2] = prompt('How old are you?', ''); */
+// ================================================
+/* const category = 'toys';
+
+console.log(`category ${category}`); */
+// ==================================================
+/* let incr = 10, 
+    decr = 10; */
+/* incr++;
+decr--; */
+/* ++incr;
+--decr; */
+
+/* console.log(incr++);
+console.log(decr--); */
+/* console.log(incr);
+console.log(decr); */
+// ==========================================
+
+
+
