2bb4c2e3e2f4f5d58f1c477a34d42159df2898a6
Daryna_Korotych <darynakorotych@gmail.com>
2022-05-19 12:35:42 +0300

    The first task from Petrychenko

--- js/test.js
+++ js/test.js
@@ -1,5 +1,27 @@
-/* 'use strict';
-const arr = ['a', 'b', 'c'];
+'use strict';
+let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+/* const arr = ['a', 'b', 'c'];
 const arrObj = {
     a: 'a',
     '1': 'b',
@@ -38,7 +60,7 @@ answers[2] = prompt('How old are you?', ''); */
 
 console.log(`category ${category}`); */
 // ==================================================
-/* let incr = 10, 
+/* let incr = 10,
     decr = 10; */
 /* incr++;
 decr--; */
