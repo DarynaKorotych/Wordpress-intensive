3748d8834cb281334e2f3b59f2f3ea8d16d11ae5
Daryna_Korotych <darynakorotych@gmail.com>
2022-05-30 16:38:15 +0300

    Coding exercise 3, 4

--- js/test.js
+++ js/test.js
@@ -24,7 +24,7 @@ console.log(personalMovieDB);
 
 
 
-
+// ================================================
 /* const arr = ['a', 'b', 'c'];
 const arrObj = {
     a: 'a',
@@ -64,33 +64,152 @@ answers[2] = prompt('How old are you?', ''); */
 
 console.log(`category ${category}`); */
 // ==================================================
-/* let incr = 10,
-    decr = 10; */
-/* incr++;
-decr--; */
-/* ++incr;
---decr; */
+/* for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+} */
 
-/* console.log(incr++);
-console.log(decr--); */
-/* console.log(incr);
-console.log(decr); */
-// ==========================================
-const num = 50;
+/* let result = '';
+let leng = 7;
 
-switch (num) {
-    case 49:
-        console.log('Wrong');
-        break;
-    case 100:
-        console.log('Wrong');
-        break;
-    case 50:
-        console.log('True');
-        break;
-    default:
-        console.log('Not this time');
-        break;
+for (let i = 1; i < leng; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += '*';
+
+    }
+    result += '\n';
 }
 
+console.log(result); */
 
+/* first: for (let i = 0; i < 3; i++) {
+    console.log(`First level ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k === 2) {
+                break first;
+            }
+            console.log(`Third level ${k}`);
+        }
+    }
+} */
+// 1
+
+/* for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
+//
+let num = 5;
+while (num <= 10) {
+    console.log(num);
+    num++;
+} */
+
+// 2
+/* for (let i = 20; i >= 10; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+} */
+
+// 3
+/* for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    
+} */
+
+// 4
+/* for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+// OR
+let num = 2;
+while (num <= 16) {
+    num++;
+    if (num % 2 === 0) {
+        continue;
+    } else {
+        console.log(num);
+    }
+} */
+
+
+
+// 5
+/* const arrayOfNumbers = [];
+
+
+for (let i = 5; i < 11; i++) {
+
+    for (let j = 0; j < i; j++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+}
+console.log(arrayOfNumbers); */
+
+// 1*
+/* function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+
+    }
+    console.log(result);
+
+    // Не трогаем
+    return result;
+}
+firstTask(); */
+
+// 2*
+/* function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+    for (let i = 0; i < data.length; i++) {
+            if (typeof(data[i]) === 'number') {
+                data[i] = data[i] * 2;
+            } else if (typeof(data[i]) === 'string') {
+                data[i] = `${data[i]} - done`;
+            }
+    }
+    console.log(data);
+
+    // Не трогаем
+    return data;
+}
+secondTask(); */
+
+// 3*
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i];
+        
+    }
+    console.log(result);
+
+    // Не трогаем
+    return result;
+}
+thirdTask();
