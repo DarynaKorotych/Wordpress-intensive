e79e9e9975d478d1c14cc3cc54a44899cfb6b82d
Daryna_Korotych <darynakorotych@gmail.com>
2022-05-30 16:57:07 +0300

    Coding exercise 5

--- js/test.js
+++ js/test.js
@@ -197,7 +197,7 @@ firstTask(); */
 secondTask(); */
 
 // 3*
-function thirdTask() {
+/* function thirdTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
     const data = [5, 10, 'Shopping', 20, 'Homework'];
     const result = [];
@@ -212,4 +212,24 @@ function thirdTask() {
     // Не трогаем
     return result;
 }
-thirdTask();
+thirdTask(); */
+
+// 1**
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' ';
+    }
+    for (let j = 0; j < i * 2 + 1; j++) {
+        result += '*';
+        
+    }
+    result += '\n';
+
+}
+console.log(result);
+
+
+
